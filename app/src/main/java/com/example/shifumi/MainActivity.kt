3f19889bb032a7837b53d919b86c3220f8247b4b
Fevre Joachim <fevre4@iutinfo-111-03.unilim.fr>
2025-02-11 15:00:28 +0100

    add title screen

--- app/src/main/java/com/example/shifumi/MainActivity.kt
+++ app/src/main/java/com/example/shifumi/MainActivity.kt
@@ -4,13 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.shifumi.ui.theme.ShiFuMiTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +26,41 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ShiFuMiTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                TitleScreen()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun TitleScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Text(
+            text = stringResource(R.string.title_title_screen),
+            modifier = Modifier
+                .align(Alignment.TopCenter)
+                .padding(top = 20.dp),
+            style = TextStyle(fontSize = 50.sp)
+        )
+
+        Button(
+            onClick = {
+                // TODO
+            },
+            modifier = Modifier
+                .align(Alignment.Center)
+        ) {
+            Text(text = "ah batar tu click")
+        }
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+@Preview
+fun TitleScreenPreview() {
     ShiFuMiTheme {
-        Greeting("Android")
+        TitleScreen()
     }
 }
